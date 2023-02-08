e8045eabfb58b5f633475063292caaee77c2ec4d
Camila Paladines <camilapaladines27@gmail.com>
2023-02-07 20:23:14 -0500

    Start with layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,59 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
-	return <div className='App'></div>;
+	const expression = '1+2';
+	return (
+		<div className='app'>
+			<div className='content'>
+				<header className='header'>
+					<h1>calc</h1>
+					<div className='theme-selector'>
+						<p>THEME</p>
+						<div className='switch'>
+							<ul className='switch-numbers'>
+								<li>1</li>
+								<li>2</li>
+								<li>3</li>
+							</ul>
+							<ul className='switch-buttons'>
+								<li>
+									<button></button>
+								</li>
+								<li>
+									<button></button>
+								</li>
+								<li>
+									<button></button>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</header>
+				<p className='expression-zone'>{expression}</p>
+				<div className='buttons-zone'>
+					<button>7</button>
+					<button>8</button>
+					<button>9</button>
+					<button className='del-button'>DEL</button>
+					<button>4</button>
+					<button>5</button>
+					<button>6</button>
+					<button>+</button>
+					<button>1</button>
+					<button>2</button>
+					<button>3</button>
+					<button>-</button>
+					<button>.</button>
+					<button>0</button>
+					<button>/</button>
+					<button>x</button>
+					<button className='reset-button'>RESET</button>
+					<button className='equal-button'>=</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
