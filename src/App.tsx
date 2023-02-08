983518e6c4de9d8ded65ac67f366ca9429f5c280
Camila Paladines <camilapaladines27@gmail.com>
2023-02-07 21:09:32 -0500

    Add colors theme 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import './App.scss';
 
 function App() {
 	const expression = '1+2';
+	const theme = 1;
+
 	return (
-		<div className='app'>
-			<div className='content'>
-				<header className='header'>
+		<div className={`app-${theme}`}>
+			<div className={`content-${theme}`}>
+				<header className={`header-${theme}`}>
 					<h1>calc</h1>
-					<div className='theme-selector'>
+					<div className={`theme-selector-${theme}`}>
 						<p>THEME</p>
-						<div className='switch'>
-							<ul className='switch-numbers'>
+						<div className={`switch-${theme}`}>
+							<ul className={`switch-numbers-${theme}`}>
 								<li>1</li>
 								<li>2</li>
 								<li>3</li>
 							</ul>
-							<ul className='switch-buttons'>
+							<ul className={`switch-buttons-${theme}`}>
 								<li>
 									<button></button>
 								</li>
@@ -30,26 +32,26 @@ function App() {
 						</div>
 					</div>
 				</header>
-				<p className='expression-zone'>{expression}</p>
-				<div className='buttons-zone'>
-					<button>7</button>
-					<button>8</button>
-					<button>9</button>
-					<button className='del-button'>DEL</button>
-					<button>4</button>
-					<button>5</button>
-					<button>6</button>
+				<p className={`screen-${theme}`}>{expression}</p>
+				<div className={`keypad-${theme}`}>
+					<button className={`key-${theme}`}>7</button>
+					<button className={`key-${theme}`}>8</button>
+					<button className={`key-${theme}`}>9</button>
+					<button className={`del-key-${theme}`}>DEL</button>
+					<button className={`key-${theme}`}>4</button>
+					<button className={`key-${theme}`}>5</button>
+					<button className={`key-${theme}`}>6</button>
 					<button>+</button>
-					<button>1</button>
-					<button>2</button>
-					<button>3</button>
+					<button className={`key-${theme}`}>1</button>
+					<button className={`key-${theme}`}>2</button>
+					<button className={`key-${theme}`}>3</button>
 					<button>-</button>
 					<button>.</button>
-					<button>0</button>
+					<button className={`key-${theme}`}>0</button>
 					<button>/</button>
 					<button>x</button>
-					<button className='reset-button'>RESET</button>
-					<button className='equal-button'>=</button>
+					<button className={`reset-key-${theme}`}>RESET</button>
+					<button className={`equal-key-${theme}`}>=</button>
 				</div>
 			</div>
 		</div>
